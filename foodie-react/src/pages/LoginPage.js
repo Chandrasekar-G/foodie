ba4f746ade869d6aa8389eff15257e0a55e44c3a
Chandrasekar-G <chandrasekarg@outlook.com>
2020-01-20 12:48:17 +0530

    Login page works with react

---
+++ foodie-react/src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/loginPageStyles.css";
+
+class LoginPage extends React.Component {
+  state = {};
+  render() {
+    return <div>Login Page works!</div>;
+  }
+}
+
+export default LoginPage;
