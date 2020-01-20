a29593c00ce16dadfe71bea6e7f5a9fc978b2ad6
Chandrasekar-G <chandrasekarg@outlook.com>
2020-01-20 13:06:48 +0530

    Login Page component

--- foodie-react/src/pages/LoginPage.js
+++ foodie-react/src/pages/LoginPage.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "../styles/loginPageStyles.css";
+import logo from "../resources/images/logo.png";
 
-class LoginPage extends React.Component {
-  state = {};
+export default class LoginPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: "",
+      password: ""
+    };
+  }
+
   render() {
-    return <div>Login Page works!</div>;
+    return (
+      <div className="main-container login-page">
+        <div className="login-background"></div>
+        <div className="login-form">
+          <div className="app-logo">
+            <img src={logo} />
+          </div>
+          <form className="login-form" action="">
+            <h1>Sign In</h1>
+            <div className="form-field">
+              
+            </div>
+            <div className="form-field">
+              
+            </div>
+            <div className="form-field">
+              
+            </div>
+            <span className="forgot-password">Forgot Password?</span>
+            <a className="link" href="">
+              Click Here
+            </a>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
-
-export default LoginPage;
